44fab0f3408b5289e591eb897a11f3e5955ea185
S.M Zubair <Devilhunter341@gmail.com>
2016-05-25 01:35:19 +0500

    adding style

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,7 +15,7 @@ import android.widget.Toast;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 99;
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +69,18 @@ public class MainActivity extends AppCompatActivity {
 
         int price = calculatePrice(hasWhippedCream,hasChocolate);
         String priceMessage =createOrderSummary(name,price,hasWhippedCream,hasChocolate);
-        displayMessage(priceMessage);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "just java orders for " + name );
+        intent.putExtra(Intent.EXTRA_TEXT,   priceMessage );
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+
+
+
 
 
 
@@ -118,13 +131,6 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView order_summary_text_view = (TextView) findViewById(R.id.order_summary_text_view);
-        order_summary_text_view.setText(message);
-    }
 
 
 
